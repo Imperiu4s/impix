357a35cdbbb5a4030aaed1cf4af1b1b847c52d5f
Imperius <airfeedx@gmail.com>
2026-07-12 21:54:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const movies = [
         id: "movie1",
         title: "K-Pop Démon Vadászok",
         description: "Amikor Rumi, Mira és Zoey K-pop-szupersztárok koncertje nem telt házas, a titkos képességeiket használják, hogy megvédjék rajongóikat a természetfeletti fenyegetésektől.",
-        thumbnail: "../assets/kpop.png",
+        thumbnail: "assets/kpop.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=zYJUZZ0GBZjuZJPf",
         year: "2025",
@@ -31,8 +31,8 @@ const movies = [
         id: "movie2",
         title: "A kém",
         description: "Susan Cooper, a háttérben dolgozó CIA-elemző kénytelen terepre lépni, hogy megakadályozza egy nukleáris fegyver eladását, miközben veszélyes bűnözőket és kettős játékot játszó ügynököket próbál leleplezni.",
-        thumbnail: "../assets/a_kém.png",
-        isNew: true,
+        thumbnail: "assets/a_kém.png",
+        isNew: false,
         iframe: "https://videa.hu/player?v=IdWDoVY6krd7gBkM",
         year: "2015",
         age: "16+"
@@ -41,7 +41,7 @@ const movies = [
         id: "movie3",
         title: "365 nap",
         description: "Egy nő egy befolyásos maffiafőnök áldozatául esik, aki elrabolja, és egy évet ad neki, hogy beleszeressen.",
-        thumbnail: "../assets/365nap.png",
+        thumbnail: "assets/365nap.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=YAeRUsWR2JC81m1S",
         year: "2020",
@@ -51,7 +51,7 @@ const movies = [
         id: "movie4",
         title: "365 nap: Ma",
         description: "Laura és Massimo visszatér, és erősebb, mint valaha. De Massimo családi kötelékei és a Laura szívére pályázó titokzatos férfi megnehezítik a szerelmesek életét.",
-        thumbnail: "../assets/365napma.png",
+        thumbnail: "assets/365napma.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=XuCWhGFzj17ezFnM",
         year: "2022",
@@ -61,7 +61,7 @@ const movies = [
         id: "movie5",
         title: "365 nap: Egy újabb nap",
         description: "Laura és Massimo kapcsolata egy hajszálon függ, miközben próbálják megoldani bizalmi problémáikat, Nacho pedig kitartóan azon ügyködik, hogy elszakítsa őket egymástól.",
-        thumbnail: "../assets/365napegyújabbnap.png",
+        thumbnail: "assets/365napegyújabbnap.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=cN8MNjmVFJK7sUmk",
         year: "2022",
@@ -71,7 +71,7 @@ const movies = [
         id: "movie6",
         title: "Sokkal több mint testőr",
         description: "Egy felhajtást kerülő testőrnek életben kell tartania egy sztártanút – aki történetesen egy lobbanékony bérgyilkos –, hogy vallomást tehessen egy brutális diktátor ellen.",
-        thumbnail: "../assets/sokkal_több_mint_testőr_1.png",
+        thumbnail: "assets/sokkal_több_mint_testőr_1.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=shuMFmlaiNE41Wlh",
         year: "2017",
@@ -81,7 +81,7 @@ const movies = [
         id: "movie7",
         title: "Sokkal több mint testőr 2",
         description: "A testőr Michael Bryce Darius Kincaid bérgyilkossal és annak Sonia nevű feleségével közösen belekeveredik egy globális összeesküvésbe ebben a fergeteges vígjátékban.",
-        thumbnail: "../assets/sokkal_több_mint_testőr_2.png",
+        thumbnail: "assets/sokkal_több_mint_testőr_2.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=tEuZPQAFuaoAPAqF",
         year: "2021",
@@ -91,7 +91,7 @@ const movies = [
         id: "movie8",
         title: "Nász-ajánlat",
         description: "Egy könyvkiadó idegesítő főszerkesztője megtudja, hogy elutasították a vízumkérelmét, és ki fogják toloncolni az országból, ezért rákényszeríti asszisztensét, hogy feleségül vegye.",
-        thumbnail: "../assets/nász_ajánlat.png",
+        thumbnail: "assets/nász_ajánlat.png",
         isNew: false,
         iframe: "https://videa.hu/player?v=gI6XQUhA7wIyxVZY",
         year: "2009",
@@ -101,7 +101,7 @@ const movies = [
         id: "movie9",
         title: "Amerika kapitány: Az első bosszúálló",
         description: "Amerika Kapitány szuperkatonává válik, és legyőzi a HYDRA vezetőjét, miközben megmenti a világot. Önfeláldozása után évtizedekkel később a modern korban ébred fel.",
-        thumbnail: "../assets/akelsobosszuallo.png",
+        thumbnail: "assets/akelsobosszuallo.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=gieXTQt3qIVhqxsW",
         year: "2011",
@@ -111,7 +111,7 @@ const movies = [
         id: "movie10",
         title: "Amerika kapitány: Szép új világ",
         description: "Amerika Kapitány szuperkatonává válik, és legyőzi a HYDRA vezetőjét, miközben megmenti a világot. Önfeláldozása után évtizedekkel később a modern korban ébred fel.",
-        thumbnail: "../assets/akszepujvilag.png",
+        thumbnail: "assets/akszepujvilag.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=Ys4NCuliTxIzpdId",
         year: "2025",
@@ -121,7 +121,7 @@ const movies = [
         id: "movie11",
         title: "Női szervek",
         description: "Az FBI-ügynök Sarah Ashburn és a bostoni rendőrnő Shannon Mullins teljesen különbözőek, ezért ki nem állhatják egymást. Egy veszélyes drogbáró elfogásához azonban össze kell fogniuk, és a közös nyomozás során megtanulják tisztelni egymást.",
-        thumbnail: "../assets/női_szervek.png",
+        thumbnail: "assets/női_szervek.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=4HSRvSpmcCE6fHZb",
         year: "2013",
@@ -131,7 +131,7 @@ const movies = [
         id: "movie12",
         title: "Apáca show",
         description: "Whoopi Goldberg egy bárénekesnőt alakít, aki a maffia elől egy kolostorba menekül. Ott apácának álcázva váratlanul felvirágoztatja a kórust, de a sikere veszélybe sodorja a titkát.",
-        thumbnail: "../assets/apácashow1.png",
+        thumbnail: "assets/apácashow1.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=m47PT2v2exM4EnvO",
         year: "1992",
@@ -141,7 +141,7 @@ const movies = [
         id: "movie13",
         title: "Apáca show 2.",
         description: "Deloris ismét Mary Clarence bőrébe bújik, hogy egy iskolában zenetanárként problémás diákokból kórust formáljon, és megmentse az intézményt a bezárástól.",
-        thumbnail: "../assets/apácashow2.png",
+        thumbnail: "assets/apácashow2.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=dbfIjPOfN6ewejwD",
         year: "1993",
@@ -151,7 +151,7 @@ const movies = [
         id: "movie14",
         title: "Tavaszi szél",
         description: "A dokumentumfilm Magyar Péter politikai mozgalmának születését követi, egy éven át bemutatva a kampányokat és a nyilvánosság előtt eddig nem látott személyes pillanatokat.",
-        thumbnail: "../assets/tavasziszél.png",
+        thumbnail: "assets/tavasziszél.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=rIoRmMchuCbuwiAr",
         year: "2026",
@@ -161,11 +161,21 @@ const movies = [
         id: "movie15",
         title: "Randiguru",
         description: "Egy jóképű New York-i randiguru tőle szokatlan módon őrülten belehabarodik egy okos és cinikus riporterbe, aki immunisnak tűnik a sármjára.",
-        thumbnail: "../assets/randiguru.png",
+        thumbnail: "assets/randiguru.png",
         isNew: true,
         iframe: "https://videa.hu/player?v=68lVVGrtMDuDZoTK",
         year: "2005",
         age: "13+"
+    },
+    {
+        id: "movie16",
+        title: "Családi üzelmek",
+        description: "Hogy törlessze adósságát egy drogbárónak, egy fűdíler meggyőzi a szomszédjait, játsszák el a családját és segítsenek neki átcsempészni egy nagy szállítmányt a határon.",
+        thumbnail: "assets/családi_üzelmek.png",
+        isNew: true,
+        iframe: "https://videa.hu/player?v=RUnCkLkGwsoD0EHu",
+        year: "2013",
+        age: "16+"
     },
 ];
 
@@ -174,7 +184,7 @@ const series = [
         id: "series1",
         title: "Stranger Things",
         description: "Egy fiatal fiú eltűnését követően a kisváros lakói titkos kísérletekre, rémisztő természetfeletti erőkre és egy furcsa kislányra derítenek fényt.",
-        thumbnail: "../assets/stranger_things.png",
+        thumbnail: "assets/stranger_things.png",
         isNew: false,
         year: "2025",
         age: "16+",
@@ -192,7 +202,7 @@ const series = [
         id: "series2",
         title: "Ginny és Georgia",
         description: "A szabad szellemű Georgia két gyerekével, Ginnyvel és Austinnal északra költözik, hogy új életet kezdjenek, azonban az új kezdethez vezető út rögösnek bizonyul.",
-        thumbnail: "../assets/ginny_and_georgia.png",
+        thumbnail: "assets/ginny_and_georgia.png",
         isNew: true,
         year: "2025",
         age: "16+",
@@ -248,7 +258,7 @@ const series = [
         id: "series3",
         title: "Modern család",
         description: "A modern család három különböző család életét mutatja be egy dokumentumfilmes stáb kameráján keresztül. Ennek a bonyolult, zűrös, szerető és modern családnak Jay Pritchett a feje.",
-        thumbnail: "../assets/modern_család.png",
+        thumbnail: "assets/modern_család.png",
         isNew: false,
         year: "2009",
         age: "12+",
@@ -304,7 +314,7 @@ const series = [
         id: "series4",
         title: "Kémbarátok",
         description: "Egy NSA-ügynök megbízást kap, hogy összekötőként működjön a brit kormányzati kommunikációs központ kiberbűnözés elleni részlegénél, és hamar ellenszenvessé teszi az egység vezetőjét pimasz stílusával és azzal a hajlamával, hogy megpróbálja átvenni a hatalmat.",
-        thumbnail: "../assets/kémbarátok.png",
+        thumbnail: "assets/kémbarátok.png",
         isNew: true,
         year: "2020",
         age: "14+",
@@ -464,7 +474,7 @@ function checkSessionAndPassword() {
 
         // 2. ÉLŐ FIGYELŐ: Nem csak egyszer kérjük le, hanem folyamatosan fülelünk!
         const passwordRef = ref(database, 'passwords/' + lockedPassword);
-        
+
         // Az onValue azonnal észreveszi, ha a Firebase-ben módosítasz vagy TÖRLÖD a jelszót
         onValue(passwordRef, (snapshot) => {
             const passwordData = snapshot.val();
@@ -481,8 +491,16 @@ function checkSessionAndPassword() {
             const expiration = new Date(year, month - 1, day, 23, 59, 59).getTime();
 
             if (liveCurrentTime > expiration) {
-                forceLogout(true);
+                // Amint a háttérben lejárt az idő, a JS azonnal törli EZT AZ EGY jelszót
+                update(ref(database, 'passwords'), {
+                    [lockedPassword]: null
+                }).then(() => {
+                    forceLogout(true);
+                }).catch((err) => {
+                    console.error("Háttérbeni törlési hiba:", err);
+                });
             } else {
+
                 // Görgetés menedzsment (a modal állapota szerint)
                 const modal = document.getElementById('modal');
                 const isModalOpen = modal && (modal.style.display === 'block' || modal.classList.contains('active') || !modal.classList.contains('hidden'));
@@ -556,10 +574,16 @@ function checkPassword() {
         const expiration = new Date(year, month - 1, day, 23, 59, 59).getTime();
 
         if (currentTime > expiration) {
-            // Előbb megmutatjuk az ablakot, és CSAK az OK gomb megnyomása UTÁN ürítjük a mezőt!
-            showImpixAlert("Az előfizetésed lejárt! Fizess elő újból és regisztráld újra jelszavad hogy használhasd a streaming szolgáltatásunkat!", () => {
-                inputField.value = '';
-                inputField.focus();
+            // CSAK ezt az egy lefutott jelszót töröljük a Firebase-ből azzal, hogy null-ra frissítjük
+            update(ref(database, 'passwords'), {
+                [enteredPassword]: null
+            }).then(() => {
+                showImpixAlert("Az előfizetésed lejárt! Fizess elő újból és regisztráld újra jelszavad hogy használhasd a streaming szolgáltatásunkat!", () => {
+                    inputField.value = '';
+                    inputField.focus();
+                });
+            }).catch((err) => {
+                console.error("Hiba az automatikus törlés során:", err);
             });
             return;
         }
@@ -626,7 +650,7 @@ function getOrCreateDeviceId() {
         // Generálunk egy teljesen egyedi, véletlenszerű karaktersorozatot (UUID stílusban)
         const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         const timestamp = new Date().getTime();
-        
+
         deviceId = 'impix_device_' + randomString + '_' + timestamp;
 
         // Ezt most elmentjük a gépén örökre (túléli az újraindítást, böngésző bezárást)
